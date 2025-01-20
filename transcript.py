cc1a28c9b8dd2551ff28cefed113b3e5d8b205a7
IIIThoid3rd <numxtzu.3rd@gmail.com>
2025-01-20 23:11:33 +0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -3,9 +3,3 @@ import sys
 import time
 import os
 
-def newStudentFeature():
-    print("Preparing for a new student...")
-    time.sleep(2)
-    print("Redirecting you to the main menu...")
-    time.sleep(2)
-    startFeature()
