8b297974fa2110f7ef6abb0eb01cda363f8099b3
IIIThoid3rd <numxtzu.3rd@gmail.com>
2025-01-20 23:06:43 +0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,3 +1,9 @@
 import csv
 import sys
 import time
+def newStudentFeature():
+    print("Preparing for a new student...")
+    time.sleep(2)
+    print("Redirecting you to the main menu...")
+    time.sleep(2)
+    startFeature()
